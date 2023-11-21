bfe1dd5b257eec771b010e32a7224d7d5cb0d419
Lyrceo <73700118+Lyrceo@users.noreply.github.com>
2023-11-21 22:50:29 +0200

    first126 commit

--- lesson1111.py
+++ lesson1111.py
@@ -1,9 +1,18 @@
-import cv2
+import requests
+from bs4 import BeautifulSoup as bs
 
-image_path = 'cat.jpg'
-cat_face_cascade = cv2.CascadeClassifier('nn.xml')
-image = cv2.imread(image_path)
-cat_face = cat_face_cascade.detectMultiScale(image)
-print(cat_face)
-cv2.imshow('Cat', image)
-cv2.waitKey()
+re1 = requests.get("https://sinoptik.ua/")
+html = bs(re1.text, "html.parser")
+data = html.find_all('div', class_='max')
+data1 = html.find_all('p', class_='day-link')
+data2 = html.find_all('a', class_='day-link')
+temps = []
+days = []
+for i in data:
+  temps.append(i.span.text)
+for i in data1:
+  days.append(i.text)
+for i in data2:
+  days.append(i.text)
+for i in range(len(temps) - 1):
+  print(days[i], temps[i])
